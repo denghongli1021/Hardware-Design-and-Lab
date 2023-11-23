458e05d22c5eedb7c95086b961bd967c14cdb72f
denghongli1021 <116105955+denghongli1021@users.noreply.github.com>
2023-11-24 00:00:32 +0800

    lab7 循環播放 增加長度到512

---
+++ verilog701/verilog701.srcs/sources_1/imports/lab7_template/lab7.v
@@ -0,0 +1,159 @@
+`define silence   32'd50000000
+
+module lab7(
+    input clk,
+    input rst,        // BTNC: active high reset
+    input _play,      // SW0: Play/Pause
+    input _start,     // SW1: Start/Exit
+    input _mute,      // SW14: Mute
+    input _mode,      // SW15: Mode
+    input _volUP,     // BTNU: Vol up
+    input _volDOWN,   // BTND: Vol down
+    inout PS2_DATA,   // Keyboard I/O
+    inout PS2_CLK,    // Keyboard I/O
+    output reg [15:0] _led,       // LED: [15:9] key & [4:0] volume
+    output audio_mclk, // master clock
+    output audio_lrck, // left-right clock
+    output audio_sck,  // serial clock
+    output audio_sdin, // serial audio data input
+    output [6:0] DISPLAY,    
+    output [3:0] DIGIT
+    );        
+    
+    // Modify these
+    //reg [15:0] _led = 16'b1110_0000_0000_0111;
+    assign DIGIT = 4'b0000;
+    assign DISPLAY = 7'b0111111;
+
+    // Internal Signal
+    wire [15:0] audio_in_left, audio_in_right;
+
+    wire [11:0] ibeatNum;               // Beat counter
+    wire [31:0] freqL, freqR;           // Raw frequency, produced by music module
+    wire [21:0] freq_outL, freq_outR;    // Processed frequency, adapted to the clock rate of Basys3
+
+    // clkDiv22
+    wire clkDiv22;
+    clock_divider #(.n(22)) clock_22(.clk(clk), .clk_div(clkDiv22));    // for audio
+
+    // Player Control
+    // [in]  reset, clock, _play, _slow, _music, and _mode
+    // [out] beat number
+    player_control #(.LEN(512)) playerCtrl_00 ( 
+        .clk(clkDiv22),
+        .reset(rst),
+        ._play(_play), 
+        ._mode(_mode),
+        .ibeat(ibeatNum)
+    );
+
+    // Music module
+    // [in]  beat number and en
+    // [out] left & right raw frequency
+    music_example music_00 (
+        .ibeatNum(ibeatNum),
+        .en(1'b1),
+        .toneL(freqL),
+        .toneR(freqR)
+    );
+
+    // freq_outL, freq_outR
+    // Note gen makes no sound, if freq_out = 50000000 / `silence = 1
+    assign freq_outL = 50000000 / freqL;
+    assign freq_outR = 50000000 / freqR;
+
+    // Note generation
+    // [in]  processed frequency
+    // [out] audio wave signal (using square wave here)
+    reg [2:0] vol = 2;
+    note_gen noteGen_00(
+        .clk(clk), 
+        .rst(rst), 
+        .volume(vol),
+        .mute(_mute),
+        .play(_play),
+        .note_div_left(freq_outL), 
+        .note_div_right(freq_outR), 
+        .audio_left(audio_in_left),     // left sound audio
+        .audio_right(audio_in_right)    // right sound audio
+    );
+
+    // Speaker controller
+    speaker_control sc(
+        .clk(clk), 
+        .rst(rst), 
+        .audio_in_left(audio_in_left),      // left channel audio data input
+        .audio_in_right(audio_in_right),    // right channel audio data input
+        .audio_mclk(audio_mclk),            // master clock
+        .audio_lrck(audio_lrck),            // left-right clock
+        .audio_sck(audio_sck),              // serial clock
+        .audio_sdin(audio_sdin)             // serial audio data input
+    );
+    wire up1,up2;
+    debounce d1 (
+        .pb_debounced(up1), 
+        .pb(_volUP),
+        .clk(clk)
+    );
+    onepulse d2(
+        .signal(up1), 
+        .clk(clk), 
+        .op(up2)
+    );
+    wire dw1,dw2;
+    debounce e1 (
+        .pb_debounced(dw1), 
+        .pb(_volDOWN),
+        .clk(clk)
+    );
+    onepulse e2 (
+        .signal(dw1), 
+        .clk(clk), 
+        .op(dw2)
+    );
+    wire rst1,rst2;
+    debounce c1 (
+        .pb_debounced(rst1), 
+        .pb(rst),
+        .clk(clk)
+    );
+    onepulse c2 (
+        .signal(rst1), 
+        .clk(clk), 
+        .op(rst2)
+    );
+    
+    always@(posedge clk or posedge rst2) begin
+        if (rst2) begin
+            vol = 2;
+        end
+        else if (up2 && vol <= 3) begin
+            vol = vol + 1;
+        end
+        else if (dw2 && vol >= 1) begin
+            vol = vol - 1;
+        end
+    end
+    always@(*) begin
+        case(vol)
+        0 : begin
+            _led[4:0] = 5'b00001;
+        end
+        1 : begin
+            _led[4:0] = 5'b00011;
+        end
+        2 : begin
+            _led[4:0] = 5'b00111;
+        end
+        3 : begin
+            _led[4:0] = 5'b01111;
+        end
+        4 : begin
+            _led[4:0] = 5'b11111;
+        end
+        default : begin
+            _led[4:0] = 5'b00000;
+        end
+        endcase
+    end
+endmodule
---
+++ verilog701/verilog701.srcs/sources_1/imports/lab7_template/note_gen.v
@@ -0,0 +1,143 @@
+module note_gen(
+    input clk, // clock from crystal
+    input rst, // active high reset
+    input mute,
+    input play,
+    input [2:0] volume, 
+    input [21:0] note_div_left, // div for note generation
+    input [21:0] note_div_right,
+    output reg [15:0] audio_left,
+    output reg [15:0] audio_right
+    );
+
+    // Declare internal signals
+    reg [21:0] clk_cnt_next, clk_cnt;
+    reg [21:0] clk_cnt_next_2, clk_cnt_2;
+    reg b_clk, b_clk_next;
+    reg c_clk, c_clk_next;
+
+    // Note frequency generation
+    // clk_cnt, clk_cnt_2, b_clk, c_clk
+    always @(posedge clk or posedge rst)
+        if (rst == 1'b1)
+            begin
+                clk_cnt <= 22'd0;
+                clk_cnt_2 <= 22'd0;
+                b_clk <= 1'b0;
+                c_clk <= 1'b0;
+            end
+        else
+            begin
+                clk_cnt <= clk_cnt_next;
+                clk_cnt_2 <= clk_cnt_next_2;
+                b_clk <= b_clk_next;
+                c_clk <= c_clk_next;
+            end
+    
+    // clk_cnt_next, b_clk_next
+    always @*
+        if (clk_cnt == note_div_left)
+            begin
+                clk_cnt_next = 22'd0;
+                b_clk_next = ~b_clk;
+            end
+        else
+            begin
+                clk_cnt_next = clk_cnt + 1'b1;
+                b_clk_next = b_clk;
+            end
+
+    // clk_cnt_next_2, c_clk_next
+    always @*
+        if (clk_cnt_2 == note_div_right)
+            begin
+                clk_cnt_next_2 = 22'd0;
+                c_clk_next = ~c_clk;
+            end
+        else
+            begin
+                clk_cnt_next_2 = clk_cnt_2 + 1'b1;
+                c_clk_next = c_clk;
+            end
+
+    // Assign the amplitude of the note
+    // Volume is controlled here
+    // assign audio_left = (note_div_left == 22'd1) ? 16'h0000 : 
+    //                             (b_clk == 1'b0) ? 16'hE000 : 16'h2000;
+    // assign audio_right = (note_div_right == 22'd1) ? 16'h0000 : 
+    //                             (c_clk == 1'b0) ? 16'hE000 : 16'h2000;
+    always@(*) begin
+        if (note_div_left == 22'd1) begin
+            audio_left = 16'h0000;
+        end
+        else if (play == 0) begin
+            audio_left = 16'h0000;
+        end
+        else if (mute) begin
+            audio_left = 16'h0000;
+        end
+        else begin
+            case(volume)
+                3'd0 : begin
+                    if (b_clk == 0) audio_left = 16'hE000 >> 5;
+                    else audio_left = 16'h2000 >> 5;
+                end
+                3'd1 : begin
+                    if (b_clk == 0) audio_left = 16'hE000 >> 4;
+                    else audio_left = 16'h2000 >> 4;
+                end
+                3'd2 : begin
+                    if (b_clk == 0) audio_left = 16'hE000 >> 3;
+                    else audio_left = 16'h2000 >> 3;
+                end
+                3'd3 : begin
+                    if (b_clk == 0) audio_left = 16'hE000 >> 2;
+                    else audio_left = 16'h2000 >> 2;
+                end
+                3'd4 : begin
+                    if (b_clk == 0) audio_left = 16'hE000 >> 1;
+                    else audio_left = 16'h2000 >> 1;
+                end
+                default : begin
+                    audio_left = 0;
+                end
+            endcase
+        end
+        if (note_div_right == 22'd1) begin
+            audio_right = 16'h0000;
+        end
+        else if (play == 0) begin
+            audio_right = 16'h0000;
+        end
+        else if (mute) begin
+            audio_right = 16'h0000;
+        end
+        else begin
+            case(volume)
+                3'd0 : begin
+                    if (c_clk == 0) audio_right = 16'hE000 >> 5;
+                    else audio_right = 16'h2000 >> 5;
+                end
+                3'd1 : begin
+                    if (c_clk == 0) audio_right = 16'hE000 >> 4;
+                    else audio_right = 16'h2000 >> 4;
+                end
+                3'd2 : begin
+                    if (c_clk == 0) audio_right = 16'hE000 >> 3;
+                    else audio_right = 16'h2000 >> 3;
+                end
+                3'd3 : begin
+                    if (c_clk == 0) audio_right = 16'hE000 >> 2;
+                    else audio_right = 16'h2000 >> 2;
+                end
+                3'd4 : begin
+                    if (c_clk == 0) audio_right = 16'hE000 >>1;
+                    else audio_right = 16'h2000 >> 1;
+                end
+                default : begin
+                    audio_left = 0;
+                end
+            endcase
+        end
+    end
+endmodule
