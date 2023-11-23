761c6b1351752d1377343312a9663c7ac4c8c940
denghongli1021 <116105955+denghongli1021@users.noreply.github.com>
2023-11-23 23:07:18 +0800

    山道了

--- verilog701/verilog701.ip_user_files/sim_scripts/KeyboardCtrl_0/xcelium/run.f
+++
@@ -1,9 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../verilog701.srcs/sources_1/ip/KeyboardCtrl_0/src/Ps2Interface.v" \
-  "../../../../verilog701.srcs/sources_1/ip/KeyboardCtrl_0/src/KeyboardCtrl.v" \
-  "../../../../verilog701.srcs/sources_1/ip/KeyboardCtrl_0/sim/KeyboardCtrl_0.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
